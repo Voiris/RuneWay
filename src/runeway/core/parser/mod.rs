e11177401e5fb8f734bc239d648e6856e97f9569
Voiris <ranger404.5606@mail.ru>
2025-07-13 23:17:27 +0300

    Hello, World! RuneWay bursts in!

--- src/runeway/core/parser/mod.rs
+++ src/runeway/core/parser/mod.rs
@@ -7,8 +7,6 @@ pub use self::process::ParserProcess;
 pub fn parse_code(code: String) -> Result<Vec<Statement>, String> {
     let tokens = tokenize(code);
 
-    println!("Tokens: {}", tokens.iter().map(|t| t.to_string()).collect::<Vec<String>>().join("\n"));
-
     let mut process = ParserProcess::new(tokens);
 
     process.parse_full()
