49f51646d5cf7b51c2efd502fc79fbe1af16ff0f
Voiris <ranger404.5606@mail.ru>
2025-12-16 23:38:51 +0300

    chore: move .ftl files from `compiler/resources` to `compiler/resources/fluent/` feat(runec_fluent_messages): add fluent bundle building

--- compiler/workspaces/runec_fluent_messages/src/lib.rs
+++ compiler/workspaces/runec_fluent_messages/src/lib.rs
@@ -1,10 +1,26 @@
+use fluent::{FluentResource};
+use fluent::bundle::FluentBundle;
+use once_cell::sync::Lazy;
+use unic_langid::langids;
+
 macro_rules! include_resources {
     ($($file:expr),* $(,)?) => {
-        &[$(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../resources/", $file))),*]
+        [$(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../resources/fluent/", $file))),*]
     };
 }
 
-static FLUENT_MESSAGE_RESOURCES: &[&'static str] = include_resources![
-    "lexer_messages.ftl",
-];
+static FLUENT_BUNDLE: Lazy<FluentBundle<FluentResource, intl_memoizer::concurrent::IntlLangMemoizer>> = Lazy::new(|| {
+    let lang_ids = langids!["en-US"];
+    let mut bundle = FluentBundle::new_concurrent(lang_ids);
 
+    for resource_file_text in include_resources![
+        "lexer_messages.ftl",
+    ] {
+        let res = FluentResource::try_new(resource_file_text.into())
+            .expect("Failed to parse an FTL string.");
+        bundle.add_resource(res)
+            .expect("Failed to add FTL resources to the bundle.");
+    }
+
+    bundle
+});
