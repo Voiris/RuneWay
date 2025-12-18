b20264fcb2623627ef69ac36688882530ea509bc
Voiris <ranger404.5606@mail.ru>
2025-12-18 23:00:29 +0300

    feat: add diagnostic label kind (primary/secondary)

--- compiler/workspaces/runec_errors/src/labels.rs
+++ compiler/workspaces/runec_errors/src/labels.rs
@@ -1,11 +1,35 @@
 use std::collections::HashMap;
+use std::fmt::Display;
 use fluent::FluentValue;
 use runec_source::span::Span;
+
+#[derive(Debug)]
+pub enum DiagLabelKind {
+    Primary,
+    Secondary,
+}
+
+impl DiagLabelKind {
+    pub fn marker(&self) -> char {
+        match self {
+            DiagLabelKind::Primary => '-',
+            DiagLabelKind::Secondary => '^',
+        }
+    }
+
+    pub fn color_code(&self) -> &'static str {
+        match self {
+            DiagLabelKind::Primary => "\x1b[1;96m",
+            DiagLabelKind::Secondary => "\x1b[1;93m",
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct DiagLabel<'a> {
     pub message_id: Option<&'static str>,
     pub args: Option<HashMap<&'static str, FluentValue<'a>>>,
+    pub kind: DiagLabelKind,
     pub span: Span
 }
 
