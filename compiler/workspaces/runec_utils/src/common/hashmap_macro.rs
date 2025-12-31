8eb51bf9a0f15184d95495052533f23b30b45f32
Voiris <ranger404.5606@mail.ru>
2025-12-31 13:50:37 +0300

    feat: add optional generic types for hashmap_macro.rs (hashmap!) utility

--- compiler/workspaces/runec_utils/src/common/hashmap_macro.rs
+++ compiler/workspaces/runec_utils/src/common/hashmap_macro.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! hashmap {
-    ( $( $key:expr => $value:expr ),* $(,)? ) => {{
-        let mut map = ::std::collections::HashMap::new();
+    ($( <$K:ty, $V:ty> )? $( $key:expr => $value:expr ),* $(,)? ) => {{
+        let mut map $(: ::std::collections::HashMap<$K, $V>)? = ::std::collections::HashMap::new();
         $(
             map.insert($key, $value);
         )*
