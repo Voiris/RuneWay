916a39adf945f4c2d7792325ef01935fbecf883b
Voiris <ranger404.5606@mail.ru>
2026-01-05 02:39:47 +0300

    chore: add debug asserts to byte_pos.rs

--- compiler/workspaces/runec_source/src/byte_pos.rs
+++ compiler/workspaces/runec_source/src/byte_pos.rs
@@ -7,6 +7,7 @@ impl BytePos {
     pub const MAX: usize = u32::MAX as usize;
 
     pub const fn from_usize(n: usize) -> BytePos {
+        debug_assert!(n <= Self::MAX, "BytePos overflow");
         BytePos(n as u32)
     }
 
@@ -19,6 +20,7 @@ impl Add for BytePos {
     type Output = BytePos;
 
     fn add(self, rhs: BytePos) -> BytePos {
+        debug_assert!(self.0 <= u32::MAX - rhs.0, "BytePos overflow");
         BytePos(self.0 + rhs.0)
     }
 }
@@ -27,6 +29,7 @@ impl Add<usize> for BytePos {
     type Output = BytePos;
 
     fn add(self, rhs: usize) -> BytePos {
+        debug_assert!(rhs <= Self::MAX - self.0 as usize, "BytePos overflow");
         BytePos::from_usize(self.to_usize() + rhs)
     }
 }
@@ -35,6 +38,7 @@ impl Sub for BytePos {
     type Output = BytePos;
 
     fn sub(self, rhs: BytePos) -> BytePos {
+        debug_assert!(self.0 >= rhs.0, "BytePos underflow");
         BytePos(self.0 - rhs.0)
     }
 }
@@ -43,6 +47,7 @@ impl Sub<usize> for BytePos {
     type Output = BytePos;
 
     fn sub(self, rhs: usize) -> BytePos {
+        debug_assert!(self.0 as usize >= rhs, "BytePos underflow");
         BytePos::from_usize(self.to_usize() - rhs)
     }
 }
