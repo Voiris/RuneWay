9f35fbe3853a521550f357a4787846e112928941
Voiris <ranger404.5606@mail.ru>
2026-02-10 22:47:02 +0300

    chore(runec_parse): add comment

--- compiler/workspaces/runec_parse/src/parser/pratt.rs
+++ compiler/workspaces/runec_parse/src/parser/pratt.rs
@@ -12,8 +12,7 @@ pub const fn lbp(tok: &Token) -> u8 {
         Token::Star | Token::Slash | Token::Percent => 140,
         Token::Lt | Token::Le | Token::Gt | Token::Ge => 70,
         Token::EqEq | Token::Ne => 60,
-        // postfix
-        Token::PlusPlus | Token::MinusMinus => 160,
+        Token::PlusPlus | Token::MinusMinus => 160, // postfix
         Token::OpenParen | Token::OpenBracket | Token::OpenBrace => 170,
         _ => 0,
     }
@@ -22,7 +21,7 @@ pub const fn lbp(tok: &Token) -> u8 {
 pub const fn rbp(tok: &Token) -> u8 {
     match tok {
         Token::Bang | Token::Tilde | Token::Plus | Token::Minus => 150,
-        Token::PlusPlus | Token::MinusMinus => 160,
+        Token::PlusPlus | Token::MinusMinus => 160, // prefix
         _ => 0,
     }
 }
