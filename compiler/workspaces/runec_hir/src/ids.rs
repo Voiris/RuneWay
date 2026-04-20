f3642ce91c98b03a39d59f5e20abfdae272f2389
Voiris <ranger404.5606@mail.ru>
2026-04-20 21:31:23 +0300

    feat(runec_hir): add HirLocalId

--- compiler/workspaces/runec_hir/src/ids.rs
+++ compiler/workspaces/runec_hir/src/ids.rs
@@ -10,3 +10,17 @@ impl HirId {
         self.0 as usize
     }
 }
+
+/// Index of a local variable or parameter within a specific function body.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct HirLocalId(u32);
+
+impl HirLocalId {
+    pub fn from_usize(id: usize) -> HirLocalId {
+        assert!(id <= u32::MAX as usize, "HirLocalId overflow");
+        HirLocalId(id as u32)
+    }
+    pub fn to_usize(self) -> usize {
+        self.0 as usize
+    }
+}
