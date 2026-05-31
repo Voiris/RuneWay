f3d3173171d4170f11f359257c0b42401264e37e
Voiris <ranger404.5606@mail.ru>
2026-05-31 15:32:00 +0300

    refactor(source): refactor with cargo fmt

--- compiler/runec_proc_macro_utils/src/lib.rs
+++ compiler/runec_proc_macro_utils/src/lib.rs
@@ -1,16 +1,13 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Path};
+use syn::{Path, parse_macro_input};
 
 #[proc_macro]
 pub fn last_path_segment(input: TokenStream) -> TokenStream {
     let path = parse_macro_input!(input as Path);
 
     let Some(segment) = path.segments.last() else {
-        return syn::Error::new_spanned(
-            path,
-            "expected non-empty path"
-        )
+        return syn::Error::new_spanned(path, "expected non-empty path")
             .to_compile_error()
             .into();
     };
@@ -18,5 +15,6 @@ pub fn last_path_segment(input: TokenStream) -> TokenStream {
 
     quote!(
         #symbol
-    ).into()
+    )
+    .into()
 }
