df3355dd4eab81df9a94338a6054b5bb6c982e8e
Voiris <ranger404.5606@mail.ru>
2026-06-05 17:06:13 +0300

    feat(hir): add fallible map lookups
    Allow callers to probe HIR identifiers without panicking when an ID is outside the map.

--- compiler/runec_hir/src/map.rs
+++ compiler/runec_hir/src/map.rs
@@ -39,8 +39,16 @@ impl<'src> HirMap<'src> {
         &self.items[id.to_usize()]
     }
 
+    pub fn try_get(&self, id: HirId) -> Option<&HirItem<'src>> {
+        self.items.get(id.to_usize())
+    }
+
     pub fn get_mut(&mut self, id: HirId) -> &mut HirItem<'src> {
         &mut self.items[id.to_usize()]
+    }
+
+    pub fn try_get_mut(&mut self, id: HirId) -> Option<&mut HirItem<'src>> {
+        self.items.get_mut(id.to_usize())
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (HirId, &HirItem<'src>)> {
@@ -61,5 +69,67 @@ impl<'src> HirMap<'src> {
 impl<'src> Default for HirMap<'src> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use runec_ast::SpannedStr;
+    use runec_source::byte_pos::BytePos;
+    use runec_source::source_map::SourceId;
+    use runec_source::span::{Span, Spanned};
+
+    use crate::ids::HirId;
+    use crate::item::{HirFunction, HirItem};
+    use crate::statement::HirBlock;
+    use crate::ty::HirType;
+
+    use super::HirMap;
+
+    fn span() -> Span {
+        Span::new(
+            BytePos::from_usize(0),
+            BytePos::from_usize(0),
+            SourceId::from_usize(0),
+        )
+    }
+
+    fn function(id: HirId) -> HirItem<'static> {
+        HirItem::Function(HirFunction {
+            id,
+            name: SpannedStr::new("main", span()),
+            params: Box::new([]),
+            ret_ty: Spanned::new(HirType::Unit, span()),
+            body: HirBlock {
+                stmts: Box::new([]),
+                tail: None,
+                span: span(),
+            },
+            span: span(),
+        })
+    }
+
+    #[test]
+    fn try_get_returns_none_for_unknown_id() {
+        let map = HirMap::new();
+        assert!(map.try_get(HirId::from_usize(0)).is_none());
+    }
+
+    #[test]
+    fn try_get_mut_accesses_existing_item() {
+        let mut map = HirMap::new();
+        let id = map.reserve_id();
+        map.push(function(id));
+
+        let HirItem::Function(function) = map.try_get_mut(id).expect("function should exist")
+        else {
+            panic!("expected function");
+        };
+        function.name.node = "renamed";
+
+        assert_eq!(
+            map.try_get(id).expect("function should exist").name().node,
+            "renamed"
+        );
     }
 }
