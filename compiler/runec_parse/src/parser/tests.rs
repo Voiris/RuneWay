efd8ea74d40a4fde92ed414f7e90ad083d5369ac
Voiris <ranger404.5606@mail.ru>
2026-08-21 14:37:52 +0300

    refactor(parser): extract attribute access test helper
    Reuse a shared expected AST fixture to keep attribute access parser tests concise and consistent.

--- compiler/runec_parse/src/parser/tests.rs
+++ compiler/runec_parse/src/parser/tests.rs
@@ -17,6 +17,31 @@ fn lex_source(source_map: &SourceMap, source_id: SourceId) -> Vec<SpannedToken<'
     lexer.lex_full().unwrap()
 }
 
+fn attribute_access_expr<'src>(source_id: SourceId) -> SpannedExpr<'src> {
+    SpannedExpr::new(
+        Expr::AttributeAccess {
+            value: Box::new(SpannedExpr::new(
+                Expr::AttributeAccess {
+                    value: Box::new(SpannedExpr::new(
+                        Expr::Ident("a"),
+                        Span::new(BytePos::from_usize(0), BytePos::from_usize(1), source_id),
+                    )),
+                    name: SpannedStr::new(
+                        "b",
+                        Span::new(BytePos::from_usize(2), BytePos::from_usize(3), source_id),
+                    ),
+                },
+                Span::new(BytePos::from_usize(0), BytePos::from_usize(3), source_id),
+            )),
+            name: SpannedStr::new(
+                "c",
+                Span::new(BytePos::from_usize(4), BytePos::from_usize(5), source_id),
+            ),
+        },
+        Span::new(BytePos::from_usize(0), BytePos::from_usize(5), source_id),
+    )
+}
+
 #[test]
 fn act_parse_test() {
     let (source_map, source_id) = generate_source("act main() {}");
@@ -598,36 +623,7 @@ fn attribute_access_parse_test() {
     assert_eq!(
         parse_result.stmts,
         [SpannedStmt::new(
-            Stmt::TailExpr(SpannedExpr::new(
-                Expr::AttributeAccess {
-                    value: Box::new(SpannedExpr::new(
-                        Expr::AttributeAccess {
-                            value: Box::new(SpannedExpr::new(
-                                Expr::Ident("a"),
-                                Span::new(
-                                    BytePos::from_usize(0),
-                                    BytePos::from_usize(1),
-                                    source_id
-                                )
-                            )),
-                            name: SpannedStr::new(
-                                "b",
-                                Span::new(
-                                    BytePos::from_usize(2),
-                                    BytePos::from_usize(3),
-                                    source_id
-                                )
-                            ),
-                        },
-                        Span::new(BytePos::from_usize(0), BytePos::from_usize(3), source_id),
-                    )),
-                    name: SpannedStr::new(
-                        "c",
-                        Span::new(BytePos::from_usize(4), BytePos::from_usize(5), source_id)
-                    ),
-                },
-                Span::new(BytePos::from_usize(0), BytePos::from_usize(5), source_id)
-            ),),
+            Stmt::TailExpr(attribute_access_expr(source_id)),
             Span::new(BytePos::from_usize(0), BytePos::from_usize(5), source_id)
         )]
     );
@@ -646,44 +642,7 @@ fn attribute_access_call_parse_test() {
         [SpannedStmt::new(
             Stmt::TailExpr(SpannedExpr::new(
                 Expr::Call {
-                    callee: Box::new(SpannedExpr::new(
-                        Expr::AttributeAccess {
-                            value: Box::new(SpannedExpr::new(
-                                Expr::AttributeAccess {
-                                    value: Box::new(SpannedExpr::new(
-                                        Expr::Ident("a"),
-                                        Span::new(
-                                            BytePos::from_usize(0),
-                                            BytePos::from_usize(1),
-                                            source_id
-                                        )
-                                    )),
-                                    name: SpannedStr::new(
-                                        "b",
-                                        Span::new(
-                                            BytePos::from_usize(2),
-                                            BytePos::from_usize(3),
-                                            source_id
-                                        )
-                                    ),
-                                },
-                                Span::new(
-                                    BytePos::from_usize(0),
-                                    BytePos::from_usize(3),
-                                    source_id
-                                ),
-                            )),
-                            name: SpannedStr::new(
-                                "c",
-                                Span::new(
-                                    BytePos::from_usize(4),
-                                    BytePos::from_usize(5),
-                                    source_id
-                                )
-                            ),
-                        },
-                        Span::new(BytePos::from_usize(0), BytePos::from_usize(5), source_id)
-                    )),
+                    callee: Box::new(attribute_access_expr(source_id)),
                     args: Box::new([]),
                 },
                 Span::new(BytePos::from_usize(0), BytePos::from_usize(7), source_id)
